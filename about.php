f476e99dc83d07540369d8e15081a2d0c418e819
kurasov <kurasov@users.noreply.github.com>
2017-11-21 12:49:03 +0300

    first
    first

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE>
+<html lang="ru">
+    <head>
+        <title>kirill</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+		<?php
+		$name = 'Кирилл';
+		$age = 32;
+		$mail = 'kurasov.code@gmail.com';
+		$city = 'Москва';
+		$about = 'что-то о себе'; ?>
+		<table>
+			<tr>
+				<td>Имя</td>
+				<td><?php echo $name; ?></td>
+			</tr>
+			<tr>
+				<td>Возраст</td>
+				<td><?php echo $age; ?></td>
+			</tr>
+			<tr>
+				<td>Адрес электронной почты</td>
+				<td><?php echo $mail; ?></td>
+			</tr>
+			<tr>
+				<td>Город</td>
+				<td><?php echo $city; ?></td>
+			</tr>
+			<tr>
+				<td>О себе</td>
+				<td><?php echo $about; ?></td>
+			</tr>
+		</table>
+	</body>
